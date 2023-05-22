297c6afcd8ed5089dad9cdf92221eae2be0e637d
Ezio Lemes <46937523+eziolemes@users.noreply.github.com>
2023-05-22 09:28:08 -0400

    ajusta db

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -2,24 +2,67 @@ import express, { Response, Request } from "express";
 
 var app = express();
 
-const users = [
-  { id: 1, name: "Lucas" },
-  { id: 2, name: "Eric" },
-  { id: 3, name: "Ana" },
+const enfermeiros = [
+  {
+    "id": 1,
+    "nome": "MARIA DA SILVA",
+    "formacao_id": 2
+  },
+  {
+    "id": 2,
+    "nome": "GEOVANA OLIVEIRA",
+    "formacao_id": 1
+  },
+  {
+    "id": 3,
+    "nome": "JOÃO DA SILVA",
+    "formacao_id": 2
+  },
+  {
+    "id": 4,
+    "nome": "CAMILA FONSECA",
+    "formacao_id": 1
+  },
+  {
+    "id": 5,
+    "nome": "VITÓRIA MACHADO",
+    "formacao_id": 2
+  }
 ];
 
+const formacoes = [
+  {
+    "id": 1,
+    "nome": "TÉC. ENFERMAGEM",
+    "nivel": "TÉCNICO"
+  },
+  {
+    "id": 2,
+    "nome": "ENFERMAGEM",
+    "nivel": "SUPERIOR"
+  }
+];
 
 app.get("/", function (req: Request, res: Response) {
   res.send("Bohr Express template");
 });
 
-app.get("/users", function (req: Request, res: Response) {
-  res.send(users);
+app.get("/enfermeiros", function (req: Request, res: Response) {
+  res.send(enfermeiros);
 });
 
-app.get("/users/:userId", function (req: Request, res: Response) {
-  const user = users.find((user) => user.id === parseInt(req.params.userId));
-  res.send(user);
+app.get("/enfermeiros/:enfermeiroId", function (req: Request, res: Response) {
+  const enfermeiro = enfermeiros.find((enfermeiro) => enfermeiro.id === parseInt(req.params.enfermeiroId));
+  res.send(enfermeiro);
+});
+
+app.get("/formacoes", function (req: Request, res: Response) {
+  res.send(formacoes);
+});
+
+app.get("/formacoes/:formacaoId", function (req: Request, res: Response) {
+  const formacao = formacoes.find((enfermeiro) => formacao.id === parseInt(req.params.formacaoId));
+  res.send(formacao);
 });
 
 if (!module.parent) {
